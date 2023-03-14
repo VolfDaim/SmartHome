4796e9ce586a2b9d3370ab072de384e0728e895f
Дмитрий Литвиненко <dlitvinenko@everytag.ru>
2023-03-14 14:07:17 +0300

    http client

--- app/src/main/java/com/example/smarthome/ui/data/LoginDataSource.kt
+++ app/src/main/java/com/example/smarthome/ui/data/LoginDataSource.kt
@@ -1,6 +1,10 @@
 package com.example.smarthome.ui.data
 
 import com.example.smarthome.ui.data.model.LoggedInUser
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
 import java.io.IOException
 
 /**
@@ -10,7 +14,34 @@ class LoginDataSource {
 
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
-            // TODO: handle loggedInUser authentication
+            val params = RequestParams()
+            params.put("username", username)
+            params.put("password", password)
+
+            val client = AsyncHttpClient()
+            val url = "192.168.1.205:2000/login"
+
+            client[url, params, object : AsyncHttpResponseHandler(){
+                override fun onStart(){}
+
+                override fun onSuccess(
+                    statusCode: Int,
+                    headers: Array<Header>,
+                    responseBody: ByteArray
+                ){
+                    println(String(responseBody))
+                }
+
+                override fun onFailure(
+                    statusCode: Int,
+                    headers: Array<out Header>?,
+                    responseBody: ByteArray?,
+                    error: Throwable?
+                ) {
+                    TODO("Not yet implemented")
+                }
+            }
+            ]
             if (username == "dima" && password == "123") {
                 val fakeUser = LoggedInUser(username, password)
                 return Result.Success(fakeUser)
